2f0c7df7008b2a6d0bcd9d7ab6c632d6b2363ef0
Joao-vitor-correa-rubert <62604626+Joao-vitor-correa-rubert@users.noreply.github.com>
2020-06-19 10:34:09 -0300

    Jogodeadivinhacao

---
+++ src/dog/Dog.java
@@ -0,0 +1,10 @@
+package dog;
+public class Dog{
+        int tamanho;
+        String procriar;
+        String name;
+        
+        void latido(){
+            System.out.println("Ruff! Ruff! ");
+        }//fecha metodo latido
+}//fecha class 
---
+++ src/dog/DogTestDrive.java
@@ -0,0 +1,9 @@
+package dog;
+class DogTestDrive{
+    public static void main(String[]args){
+        Dog d = new Dog();// objeto dog 
+        
+        d.tamanho = 40;
+        d.latido();//fecha o obejto latido da class dog
+    }//fecha main
+}//fecha class 
